400d2645cd1c6b7e00d509c62ab06db48165c726
10Akshatha <157686191+10Akshatha@users.noreply.github.com>
2026-03-26 10:18:15 +0530

    first

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 DB_HOST = "localhost"
 DB_USER = "root"          # or your MySQL username
-DB_PASSWORD = "Akshu@19"   # replace with your MySQL password
+DB_PASSWORD = ""   # replace with your MySQL password
 DB_NAME = "attendance_data"      # or attendance_data if that's the one you created
